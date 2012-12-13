e08378ad7228fc071d7b80f21ce153fdbbcecc3b
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2012-12-13 15:12:18 +0100

    dllexport/import joint and body classes
    Before this commit, only METAPOD_DLL{API,IMPORT,EXPORT} was defined.
    However, there was several problems
    
    * DLLEXPORT was used in place of DLLAPI
    
    * Joint and Body classes were not exported
    
    * we do not create any metapod DLL but instead simple_arm DLL and
      simple_humanoid DLL. So we need to pass the library name to macros
      which define DLL-exported classes.
    
    This commit adds DLL API support for these two libs (and also others for
    the models we generate for the benchmarks).
    
    Support for metapod DLL API is kept for the common part, but I'm not sure it
    is working nor wise. What would happen if an application links with both
    simple_arm and simple_humanoid? Would duplicate (though identical) definitions
    create problems?
    
    This commit also moves the cmake code related simple_arm and simple_humanoid
    to each model directory. Before, it was split between the project root
    (for files listing) and the tests directory (for actual library
    creation).
    
    Moving this code was necessary in order to generate the libraries
    config.hh (the directory acts as a namespace). I moved everything for
    consistency. So the new CMakeLists.txt are there
    
        /include/metapod/models/simple-{arm,humanoid}/CMakeLists.txt
    
    If having cmake code below /include is inappropriate, we could move
    them in
    
        /simple-{arm,humanoid}/CMakeLists.txt
    
    instead. In the future, I'd like to move the models out of the
    metapod namespace. In such a case these last locations would make sense.
    Note that cureently, there is also .cc files in
    /include/metapod/models/simple-{arm,humanoid}.
    
    Eventually, this commit adds an helper function to generate config.hh.
    One cannot use the one provided by JRL-CMAKE as it can only be called from the
    project root directory.

---
+++ cmakeutils.cmake
@@ -0,0 +1,42 @@
+# Copyright 2012,
+#
+# Sébastien Barthélémy (Aldebaran Robotics)
+#
+# This file is part of metapod.
+# metapod is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# metapod is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Lesser Public License for more details.
+# You should have received a copy of the GNU Lesser General Public License
+# along with metapod.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright 2010, 2012,
+
+
+# GENERATE_CONFIG_HEADER
+#
+# Generates a configuration header for DLL API import/export
+#
+# OUTPUT: path (including filename) of the generated file. Usually the filename
+#         is config.hh
+# LIBRARY_NAME: the name of the library. It will be normalized.
+FUNCTION(GENERATE_CONFIG_HEADER OUTPUT LIBRARY_NAME)
+  STRING(REGEX REPLACE "[^a-zA-Z0-9]" "_" LIBRARY_NAME "${LIBRARY_NAME}")
+  STRING(TOLOWER "${LIBRARY_NAME}" "LIBRARY_NAME_LOWER")
+  SET(EXPORT_SYMBOL "${LIBRARY_NAME_LOWER}_EXPORTS")
+  STRING(TOUPPER "${LIBRARY_NAME}" "LIBRARY_NAME")
+  # Generate the header.
+  # LIBRARY_NAME: CPP symbol prefix, should match the compiled library name,
+  #               usually in upper case
+  # EXPORT_SYMBOl: what symbol controls the switch between symbol
+  #                import/export, usually libname_EXPORTS, with libname in
+  #                lower case.
+  CONFIGURE_FILE(
+    ${PROJECT_SOURCE_DIR}/cmake/config.hh.cmake
+    ${OUTPUT}
+    @ONLY)
+ENDFUNCTION(GENERATE_CONFIG_HEADER)
