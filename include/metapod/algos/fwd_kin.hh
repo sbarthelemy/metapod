26cbff8cdf03461be49ff8e2fe6c022a17e97ac3
Sébastien Barthélémy <sbarthelemy@aldebaran-robotics.com>
2013-04-24 14:10:56 +0200

    WIP: add a fwd_kin algorithm
    the plan for fwd_kin is to compute the forward kinematics and to remove
    duplication and inefficiencies from jcalc, bcalc and rnea.
    
    This first version adds a variation of the algorithm which does the computation
    in two steps, where the first step does not consider the root joint.
    
    It only works for models with a single tree.
    
    Change-Id: I5c695c3059af4817c34a12176e68ea19d4d5167b

---
+++ include/metapod/algos/fwd_kin.hh
@@ -0,0 +1,147 @@
+// Copyright 2013
+//
+// Sébastien Barthélémy (Aldebaran Robotics)
+//
+// This file is part of metapod.
+// metapod is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// metapod is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// You should have received a copy of the GNU Lesser General Public License
+// along with metapod.  If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * Implementation of the forward kinematics routine.
+ * This algorithm only works for models with a single root body.
+ * It updates the body global transform (iX0) using two steps:
+ *
+ *  1. fwd_kin_iX1 computes iX1 for all the bodies but the firts (root) one
+ *  2. fwd_kin_iX0_from_iX1 updates 1X0 (it is sXp for the root joint) and uses
+ *     it to compute iX0 for all the bodies.
+ *
+ * This algorithm is particularly useful for humanoid robots using the waist as
+ * the root body, parametrized with a free floating joint. One could proceed as
+ * follow:
+ *
+ * a. call jcal_pos on all joints but the root one (it updates sXp)
+ * b. call fwd_kin_iX1
+ * c. use sensor to know wich foot is in contact with the ground (let call it
+ *    f).
+ * b. update the free floating root joint from 1Xf:
+ *    1X0 = Xt0 * Xj0 = 1Xf * fX0
+ *    hence  Xj0 = Xt0.inv() * fX1.inv() * fX0
+ * e. call jcalc_pos on the root joint
+ * f. call fwd_kin_iX0_from_iX1
+ *
+ */
+
+#ifndef METAPOD_FWD_KIN_HH
+# define METAPOD_FWD_KIN_HH
+
+#include <metapod/tools/depth_first_traversal.hh>
+#include <metapod/tools/has_parent.hh>
+
+namespace metapod {
+namespace internal {
+
+template< typename Robot, int node_id, bool has_parent=true >
+struct SetBodyPose
+{
+  typedef typename Nodes<Robot, node_id>::type Node;
+  static void run(Robot& robot, Spatial::Transform *iX1)
+  {
+    Node& node = boost::fusion::at_c<node_id>(robot.nodes);
+    iX1[node_id] = node.sXp * iX1[Node::parent_id];
+  }
+};
+
+// specialization for root nodes. Normally not necessary, since
+template< typename Robot, int node_id>
+struct SetBodyPose<Robot, node_id, false>
+{
+  typedef typename Nodes<Robot, node_id>::type Node;
+  static void run(Robot& robot, Spatial::Transform *iX1)
+  {
+    Node& node = boost::fusion::at_c<node_id>(robot.nodes);
+    iX1[node_id] = node.sXp;
+  }
+};
+
+template< typename Robot, int node_id >
+struct FwdKiniX1Visitor
+{
+  typedef typename Nodes<Robot, node_id>::type Node;
+  static void discover(Robot& robot, Spatial::Transform *iX1)
+  {
+    SetBodyPose<Robot, node_id, has_parent<Robot, node_id>::value>::run(robot, iX1);
+  }
+  static void finish(Robot&, Spatial::Transform *)
+  {}
+};
+
+template< typename Robot, int node_id >
+struct FwdKiniX0FromiX1Visitor
+{
+  typedef typename Nodes<Robot, node_id>::type Node;
+  static void discover(Robot& robot, const Spatial::Transform *iX1, const Spatial::Transform &_1X0)
+  {
+    Node& node = boost::fusion::at_c<node_id>(robot.nodes);
+    node.body.iX0 = iX1[node_id] * _1X0;
+  }
+  static void finish(Robot& , const Spatial::Transform *, const Spatial::Transform &)
+  {}
+};
+} // end of namespace metapod::internal
+
+
+template< typename Robot > struct fwd_kin_iX1
+{
+  METAPOD_STATIC_ASSERT(Robot::child1_id == NO_CHILD &&
+                        Robot::child2_id == NO_CHILD &&
+                        Robot::child3_id == NO_CHILD &&
+                        Robot::child4_id == NO_CHILD,
+      "fwd_kin_iX1 only supports models with a single root link");
+  typedef typename Nodes<Robot, Robot::child0_id>::type Node;
+  // assume sXp is up to date, for all joints for the root one
+  static void run(Robot& robot, Spatial::Transform *iX1)
+  {
+    // start a depth first traversal but skip the root node
+    internal::depth_first_traversal_internal<internal::FwdKiniX1Visitor, Robot, Node::child0_id>::run(robot, iX1);
+    internal::depth_first_traversal_internal<internal::FwdKiniX1Visitor, Robot, Node::child1_id>::run(robot, iX1);
+    internal::depth_first_traversal_internal<internal::FwdKiniX1Visitor, Robot, Node::child2_id>::run(robot, iX1);
+    internal::depth_first_traversal_internal<internal::FwdKiniX1Visitor, Robot, Node::child3_id>::run(robot, iX1);
+    internal::depth_first_traversal_internal<internal::FwdKiniX1Visitor, Robot, Node::child4_id>::run(robot, iX1);
+  }
+};
+
+template< typename Robot > struct fwd_kin_iX0_from_iX1
+{
+  METAPOD_STATIC_ASSERT(Robot::child1_id == NO_CHILD &&
+                        Robot::child2_id == NO_CHILD &&
+                        Robot::child3_id == NO_CHILD &&
+                        Robot::child4_id == NO_CHILD,
+      "fwd_kin_iX0_from_iX1 only supports models with a single root link");
+  typedef typename Nodes<Robot, Robot::child0_id>::type Node;
+
+  // assume sXp is up to date for the root joint
+  static void run(Robot& robot, const Spatial::Transform iX1[])
+  {
+    Node& node = boost::fusion::at_c<Robot::child0_id>(robot.nodes);
+    node.body.iX0 = node.sXp;
+    // start a depth first traversal but skip the root node
+    internal::depth_first_traversal_internal<internal::FwdKiniX0FromiX1Visitor, Robot, Node::child0_id>::run(robot, iX1, node.body.iX0);
+    internal::depth_first_traversal_internal<internal::FwdKiniX0FromiX1Visitor, Robot, Node::child1_id>::run(robot, iX1, node.body.iX0);
+    internal::depth_first_traversal_internal<internal::FwdKiniX0FromiX1Visitor, Robot, Node::child2_id>::run(robot, iX1, node.body.iX0);
+    internal::depth_first_traversal_internal<internal::FwdKiniX0FromiX1Visitor, Robot, Node::child3_id>::run(robot, iX1, node.body.iX0);
+    internal::depth_first_traversal_internal<internal::FwdKiniX0FromiX1Visitor, Robot, Node::child4_id>::run(robot, iX1, node.body.iX0);
+  }
+};
+
+} // end of namespace metapod
+
+# endif
